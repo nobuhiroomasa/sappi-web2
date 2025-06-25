1669cce9b794eb3b9c3944241952569c964e0678
omasanobuhiro <165261264+nobuhiroomasa@users.noreply.github.com>
2025-06-25 22:03:09 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/demo-repository/',
+  // ...他設定
+});
+
 
 
 
