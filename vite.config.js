fd67df58f098f99671e435874b018f83da9599e4
nobuhiroomasa <ca01233006@st.kawahara.ac.jp>
2025-06-25 21:38:03 +0900

    訂正

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/sappi-web/', // GitHubリポジトリ名を入力
-});
+  plugins: [react()],
+})
+
+
+
