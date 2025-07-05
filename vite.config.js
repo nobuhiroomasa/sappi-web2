d632b0ddf375e7760dd86236e5913458b17ad628
nobuhiroomasa <ca01233006@st.kawahara.ac.jp>
2025-07-05 15:12:40 +0900

    ベース修正

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/sappi-web' : '/',
+  base: mode === 'production' ? '/sappi-web2' : '/',
 }));
 
 
